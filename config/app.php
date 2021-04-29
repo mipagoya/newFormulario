837ad629dd642bce5924e66eece4382b5fc6dfc3
Ever Fabian Muñoz <ever.munoz@blulogistics.com>
2021-04-29 12:45:18 -0500

    Actilizacion 5.8

--- config/app.php
+++ config/app.php
@@ -192,7 +192,7 @@ return [
         //My providers      
         Maatwebsite\Excel\ExcelServiceProvider::class,
         'Barryvdh\Debugbar\ServiceProvider',
-        Yajra\DataTables\DataTablesServiceProvider::class,
+     
         Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
@@ -246,8 +246,7 @@ return [
 
         //my aliase
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'Debugbar' => 'Barryvdh\Debugbar\Facade',
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',        
         'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
